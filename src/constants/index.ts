5dc0fa647a339bc143b9c6f071210f6a910ab87b
seungyeon.lee <dus3214@gmail.com>
2021-12-25 00:52:36 +0900

    ♻ Component refactoring

---
+++ src/constants/index.ts
@@ -0,0 +1,40 @@
+export const FUTURAMA_API_ENDPOINT = 'https://api.sampleapis.com/futurama/';
+
+export const MEDIA_QUERY_END_POINT = {
+    MOBILE: '640px',
+    TABLET: '1280px',
+    DESKTOP: '1920px',
+};
+
+export const ROUTES = [
+  {
+    ID: 0,
+    PATH: '/info',
+    LABEL: '정보',
+  },
+  {
+    ID: 1,
+    PATH: '/characters',
+    LABEL: '캐릭터',
+  },
+  {
+    ID: 2,
+    PATH: '/cast',
+    LABEL: '배역',
+  },
+  {
+    ID: 3,
+    PATH: '/episodes',
+    LABEL: '에피소드',
+  },
+  {
+    ID: 4,
+    PATH: '/questions',
+    LABEL: '퀴즈',
+  },
+  {
+    ID: 5,
+    PATH: '/inventory',
+    LABEL: '인벤토리',
+  }
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,11 @@
 import type { NextPage } from 'next'
-import useSWR from 'swr'
-import axios from 'axios'
-import type { InfoData } from '../types/info'
 
 
 const FuturamaIndexPage: NextPage = () => {
-  const url = 'https://api.sampleapis.com/futurama/info';
   
-  
-  const fetchers = (url: string) => axios.get(url).then((res) => res.data);
-  
-  const { data, error } = useSWR(url, fetchers)
-
-
-  if (error) return <div>error has occurred.</div>
-  if (!data) return <div>loading...</div>;
-
   return (
     <div>
-      {data.map((info: InfoData) => {
-        return (
-          <div key={`futurama-info-${info.synopsis}`}>
-            {info.synopsis}
-          </div>
-        )
-      })}
+      hello 
     </div>
 
   )
